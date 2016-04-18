38e877a9a717b0ad85214d4490ab3f5b0c013f9b
Jonathan Kocmoud <onlyforhisname@gmail.com>
2016-04-18 13:46:55 -0500

    Update app.py

--- Website/app.py
+++ Website/app.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template, request
 from DS_retriever import retrieve_top_n_sites_businesses
 import os
+import requests
+import copy
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     q = request.args.get('q') or ''
+    n = request.args.get('n') or 10
+    n = min(n, 50)
     results = None
+    latitude = 30.5907759
+    longitude = -96.4317468
+    locationlist = []
+    businesslist = []
     if q:
-        latitude = 44.338398
-        longitude =  -75.919733
-        n = 10
+        str = q
+        str.replace("%20", "+")
+        response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+str)
+        resp_json_payload = response.json()
+        latitude = resp_json_payload['results'][0]['geometry']['location'].get("lat", "")
+        longitude =  resp_json_payload['results'][0]['geometry']['location'].get("lng", "")
         (sites, businesses) = retrieve_top_n_sites_businesses(latitude, longitude, n, "most reviewed")
-        return render_template('index.html', sites=sites, businesses=businesses, q=q)
+        locationlist = json.dumps(sites + businesses)
+        
+        return render_template('index.html', sites=sites, businesses=businesses, q=q, latitude=latitude, longitude=longitude, locationlist=locationlist, businesslist=businesslist)
     else:
-        return render_template('index.html', results=results, q=q)
+        return render_template('index.html', results=results, q=q, latitude=latitude, longitude=longitude, locationlist=locationlist, businesslist=businesslist)
         
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))

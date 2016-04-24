3761ade31e5cd867b680621b5d59c6826c0be033
Jonathan Kocmoud <onlyforhisname@gmail.com>
2016-04-23 23:00:48 -0500

    Update clumpYelp.py

--- ReviewClumping/clumpYelp.py
+++ ReviewClumping/clumpYelp.py
@@ -8,14 +8,15 @@ import re
 def tfIdf(data, minWordSize = 3):
   dTF = {}
   dF = {}
+  biwordList = []
   for key, text in data.items():
-    wordList = re.findall(r"[\w']+", text)
+    wordList = re.findall(r"[a-zA-Z]+", text)
     dTF[key] = {}
     # 1 - Find the TF (term frequency)
     for word in wordList:
       # must be certain length
       if len(word) >= minWordSize:
-        index = word.lower()
+        index = word.lower().title()
         # try to find the word's count
         val = dTF[key].get(index, 0)
         dTF[key][index] = val + 1
@@ -24,6 +25,22 @@ def tfIdf(data, minWordSize = 3):
         if val == 0:
           val = dF.get(index, 0)
           dF[index] = val + 1
+    # Biwords
+    for i in range(len(wordList) - 1):
+      # must be certain length
+      if len(wordList[i]) >= minWordSize and len(wordList[i+1]) >= minWordSize:
+        index = ( wordList[i].lower() + " " + wordList[i+1].lower() ).title()
+        # try to find the word's count
+        val = dTF[key].get(index, 0)
+        dTF[key][index] = val + 1
+        # 2 - Find DF (doc frequency)
+        # if new, add to DF
+        if val == 0:
+          val = dF.get(index, 0)
+          dF[index] = val + 1
+          biwordItem = [key, index]
+          biwordList.append( biwordItem )
+        i += 1
   # 3 - Calculate IDF (Inverse DF) = log( #docs / df )
   dFn = len(dTF)
   for freq in dF:
@@ -43,6 +60,33 @@ def tfIdf(data, minWordSize = 3):
     # Normalize
     for tf in dTF[key]:
       dTF[key][tf] = dTF[key][tf] / dNorm
+  #combine word scores for biwords
+  for biword in biwordList:
+    words = re.findall(r"[a-zA-Z]+", biword[1] )
+    for word in words:
+      dTF[ biword[0] ][ biword[1] ] += dTF[ biword[0] ][ word ]
+    dTF[ biword[0] ][ biword[1] ] /= ( len(words) + 1 )
+  #keep only highest clump or single word
+  for biword in biwordList:
+    words = re.findall(r"[a-zA-Z]+", biword[1] )
+    keepClump = True
+    for word in words:
+      try:
+        if dTF[ biword[0] ][ biword[1] ] < dTF[ biword[0] ][ word ]:
+          keepClump = False
+      except KeyError:
+        pass
+    if keepClump:
+      for word in words:
+        try:
+          del dTF[ biword[0] ][ word ]
+        except KeyError:
+          pass
+    else:
+      try:
+        del dTF[ biword[0] ][ biword[1] ]
+      except KeyError:
+        pass
   return dTF
 
 
